393db1b696eb3e8d88c0ad9ec35a6d4a81ece943
NAKASONE kento <e225748@ie.u-ryukyu.ac.jp>
2024-09-25 15:15:55 +0900

    フッターを追加

---
+++ app/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer style={{ textAlign: 'center', padding: '10px', backgroundColor: '#f1f1f1' }}>
+      <p style={{ margin: 0 }}>&copy; enPiT 開発チームせんばる</p>
+    </footer>
+  );
+};
+
+export default Footer;
