000f8c1012391371924774ce9f6dc06f2c719ef8
NAKASONE kento <e225748@ie.u-ryukyu.ac.jp>
2024-09-25 15:25:24 +0900

    ヘッダーの色変更とリンクを追加

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Header: React.FC = () => {
   return (
-    <header style={{ display: 'flex', alignItems: 'center', padding: '10px' }}>
-      <img src="../icon.svg" alt="Logo" style={{ height: '40px', marginRight: '10px' }} />
-      <h1 style={{ margin: 0 }}>Meeting Compass</h1>
+    <header className="flex items-center p-2 bg-gray-200">
+      <Link href="/" className="flex items-center no-underline text-inherit">
+          <img src="../icon.svg" alt="Logo" className="h-10 mr-2" />
+          <h1 className="m-0">Meeting Compass</h1>
+      </Link>
     </header>
   );
 };
