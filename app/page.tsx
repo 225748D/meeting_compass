642b82b210f3450f7e39f11c59e5c8ad917a969b
NAKASONE kento <e225748@ie.u-ryukyu.ac.jp>
2024-09-26 15:02:18 +0900

    遷移後の画面に枠を追加、画面のスクロール機能の追加

--- app/page.tsx
+++ app/page.tsx
@@ -9,16 +9,17 @@ export default function Home() {
     router.push("/transcription");
   };
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-10">
       {/* 中央にアイコンを配置 */}
       <Image
         src="/icon.svg" // アイコンの画像パスを指定
         alt="Transcription Icon"
+        className="mt-8"
         width={200} // アイコンのサイズ
         height={200}
       />
 
-              {/* スタートボタン */}
+      {/* スタートボタン */}
       <button
         onClick={handleButtonClick} // ボタンが押された時の処理
         className="mt-5 px-5 py-2 text-lg rounded text-white"
@@ -27,8 +28,8 @@ export default function Home() {
       </button>
 
       {/* 説明テキスト */}
-      <p className="mt-2 text-lg text-center">
-      このアプリを使えば、音声を簡単かつ効率的にテキストに書き起こせます。
+      <p className="mt-2 text-lg text-center mb-5">
+        このアプリを使えば、音声を簡単かつ効率的にテキストに書き起こせます。
       </p>
     </div>
   );
