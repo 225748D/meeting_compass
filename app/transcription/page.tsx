770f83de0c9de9fae83303d0caf4e2e16a723304
NAKASONE kento <e225748@ie.u-ryukyu.ac.jp>
2024-10-01 17:51:35 +0900

    pip機能の追加 WIP

--- app/transcription/page.tsx
+++ app/transcription/page.tsx
@@ -15,6 +15,115 @@ async function getMicrophoneStream(): Promise<MediaStream> {
   }
 }
 
+// pipのセットアップ
+const setup = async (pipSource:HTMLDivElement, pipButton:HTMLButtonElement) => {
+  // 描画する要素のサイズを取得したい
+  // 画像の読み込みが終わるまで待つ
+  const waitingImgList = Array.from(pipSource.querySelectorAll("img"));
+  await Promise.all(
+    waitingImgList.map(
+      (el) =>
+        new Promise<void>((resolve) => {
+          el.addEventListener("load", () => resolve());
+        })
+    )
+  );
+
+  // 描画する要素のサイズを取得する
+  // 注意：重い処理なので頻繁に呼び出さないように
+  const { width, height } = pipSource.getBoundingClientRect();
+
+  // svgタグを作る
+  // SVGはHTMLではないので作り方がちょっと違う
+  // 決められたNS(Namespace)を指定して作る
+  const ns = "http://www.w3.org/2000/svg";
+  const svg = document.createElementNS(ns, "svg");
+  svg.setAttribute("width", width.toString());
+  svg.setAttribute("height", height.toString()
+  );
+
+  // foreignObjectを作る
+  // foreignObjectはその中身の描画を
+  // SVG自身ではなく外側（ここではブラウザ）に任せる
+  const foreignObject = document.createElementNS(ns, "foreignObject");
+  foreignObject.setAttribute("width", width.toString());
+  foreignObject.setAttribute("height", height.toString());
+
+  // 表示したい要素のコピーを作る
+  // xmlとしてのNSを追加
+  const html = pipSource.cloneNode(true);
+  // html.style.display = "content";
+
+  // 画像をすべてDataURLに置き換える
+  const imgList = Array.from(pipSource.querySelectorAll("img"));;
+  await Promise.all(
+    imgList.map(async (el) => {
+      const data = await fetch(el.src).then((res) => res.blob());
+      const reader = new FileReader();
+      const url :string = await new Promise((resolve) => {
+        reader.onload = () => resolve(reader.result as string);
+        reader.readAsDataURL(data);
+      });
+      el.src = url;
+      return el.decode();
+    })
+  );
+
+  // 描画に必要そうなものを作っておく
+  const style = document.createElement("style");
+  const css = await fetch("style.css").then((res) => res.text());
+  style.textContent = css;
+
+  // 組み立てる
+  html.appendChild(style);
+  foreignObject.appendChild(html);
+  svg.appendChild(foreignObject);
+
+  // svgを文字列化してURL化する
+  const svgStr = new XMLSerializer().serializeToString(svg);
+  const svgUrl =
+    "data:image/svg+xml;charset=utf-8," + encodeURIComponent(svgStr);
+
+  // svgを画像として読み込む
+  const img = new window.Image(width, height); // 修正: 'window'を追加
+  img.src = svgUrl;
+  await img.decode();
+
+  // canvasを作る
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+  if (!ctx){return}
+  canvas.width = width;
+  canvas.height = height;
+
+  (function render() {
+    ctx.clearRect(0, 0, width, height);
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+    requestAnimationFrame(() => render());
+  })();
+
+  // canvasを動画として取得
+  const stream = canvas.captureStream(1);
+
+  // canvasを表示するだけのvideo要素を作る
+  const video = document.createElement("video");
+  video.autoplay = true;
+  video.muted = true;
+  video.playsInline = true;
+  video.width = width;
+  video.height = height;
+  video.srcObject = stream;
+
+  await new Promise<void>((resolve) => {
+    video.ontimeupdate = () => {
+      resolve();
+    };
+    video.play();
+  });
+
+  return video;
+};
+
 export default function Home() {
   const [recorder, setRecorder] = useState<AudioRecorder | null>(null);
   const [speechTexts, setSpeechTexts] = useState<string[]>([]);
@@ -22,6 +131,8 @@ export default function Home() {
   const [isRecording, setIsRecording] = useState<boolean>(false);
 
   const speechTextsRef = useRef(speechTexts);
+  const topicRef = useRef<HTMLDivElement>(null);
+  const ButtonRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     speechTextsRef.current = speechTexts;
@@ -46,6 +157,28 @@ export default function Home() {
         observer.observe(scrollable, { childList: true, subtree: true });
       }
     }
+
+    (async function main() {
+      console.log("main")
+      const video = await setup(topicRef.current!, ButtonRef.current!);
+      if (!video){return}
+      console.log("video called")
+      // pipButton.style.opacity = 1;
+    
+      video.onenterpictureinpicture = () => {
+        video.style.display = "none";
+      };
+    
+      video.onleavepictureinpicture = () => {
+        video.remove();
+      };
+    
+      ButtonRef.current!.addEventListener("click", (event) => {
+        document.body.appendChild(video);
+        video.play();
+        video.requestPictureInPicture();
+      });
+    })();
 
     // 初回実行
     scrollToBottom();
@@ -106,7 +239,7 @@ export default function Home() {
       />
 
       {/* トピックを表示する枠 */}
-      <div className="mt-8 p-4 bg-white rounded shadow-lg w-3/4">
+      <div className="mt-8 p-4 bg-white rounded shadow-lg w-3/4" ref={topicRef}>
         <h2 className="text-xl font-bold mb-2 text-center">Extracted Topics</h2>
         <div className="h-24 border border-gray-300 rounded p-2 text-gray-500 flex items-center justify-center">
           {topics.length <= 0 ? (
@@ -116,6 +249,9 @@ export default function Home() {
           )}
         </div>
       </div>
+        <button ref={ButtonRef}>
+          pipボタン
+        </button>
 
       {/* 文字起こしのログを表示する枠 */}
       <div className="mt-4 mb-4 p-4 bg-white rounded shadow-lg w-3/4 max-h-96 overflow-y-auto">
