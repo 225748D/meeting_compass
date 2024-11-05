9ce3138c84a18745e84e3218c97e9bc80dc6029e
yakumi <45628320+medicine-t@users.noreply.github.com>
2024-11-05 17:20:31 +0900

    Revert "Feature/#12/introduce vad (#13)"
    This reverts commit e29c8ff8164408a0d71e6d95a9f8f9b6504bd047.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const handleButtonClick = () => {
+    router.push("/transcription");
+  };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-10">
       {/* 中央にアイコンを配置 */}
@@ -16,12 +20,12 @@ export default function Home() {
       />
 
       {/* スタートボタン */}
-      <Link
-        href={"/transcription"}
+      <button
+        onClick={handleButtonClick} // ボタンが押された時の処理
         className="mt-5 px-5 py-2 text-lg rounded text-white"
       >
         <Image src="/assets/play.svg" alt="Play" width={70} height={70} />
-      </Link>
+      </button>
 
       {/* 説明テキスト */}
       <p className="mt-2 text-lg text-center mb-5">
