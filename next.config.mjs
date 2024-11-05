97d3d1a13235e0803a466064a9a21c483c781bfb
yakumi <45628320+medicine-t@users.noreply.github.com>
2024-11-05 17:20:59 +0900

    Revert "Feature/#12/introduce vad (#13)" (#14)
    This reverts commit e29c8ff8164408a0d71e6d95a9f8f9b6504bd047.

--- next.config.mjs
+++ next.config.mjs
@@ -1,44 +1,4 @@
-import CopyPlugin from "copy-webpack-plugin";
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  experimental: {
-    esmExternals: true, // 外部モジュールをESMとして扱う
-  },
-  webpack: (config, options) => {
-    // MJSファイルの処理
-    config.module.rules.push({
-      test: /\.mjs$/,
-      type: "javascript/auto",
-    });
-
-    config.experiments = {
-      ...config.experiments,
-      asyncWebAssembly: true, // WASMの非同期読み込み
-      topLevelAwait: true, // トップレベルでのawaitを許可
-    };
-    if (!options.isServer) {
-      config.plugins.push(
-        new CopyPlugin({
-          patterns: [
-            {
-              from: "node_modules/@ricky0123/vad-web/dist/vad.worklet.bundle.min.js",
-              to: "../public/[name][ext]",
-            },
-            {
-              from: "node_modules/@ricky0123/vad-web/dist/*.onnx",
-              to: "../public/[name][ext]",
-            },
-            {
-              from: "node_modules/onnxruntime-web/dist/*.wasm",
-              to: "static/chunks/[name][ext]",
-            },
-          ],
-        })
-      );
-    }
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
