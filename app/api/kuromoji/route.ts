63d949d0f4e0ddc0018959bf000c1e4e94fda420
Takuya Takamine <e225717@ie.u-ryukyu.ac.jp>
2024-12-02 22:43:53 +0900

    サーバー上でも動作するように変更?

--- app/api/kuromoji/route.ts
+++ app/api/kuromoji/route.ts
@@ -1,9 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import kuromoji from "kuromoji";
+import path from "path";
 
 // kuromojiトークナイザーのビルダーを初期化
 const tokenizerBuilder = kuromoji.builder({
-  dicPath: "node_modules/kuromoji/dict",
+  dicPath: path.join(process.cwd(), "public") + "/kuromoji/dict",
 });
 
 export async function POST(req: NextRequest) {
@@ -22,16 +23,17 @@ export async function POST(req: NextRequest) {
     const { text } = await req.json(); // この行は単一のテキストを期待する
 
     // トークナイザーをビルド
-    const tokenizer: kuromoji.Tokenizer<kuromoji.IpadicFeatures> = await new Promise((resolve, reject) => {
-      tokenizerBuilder.build((err, tokenizer) => {
-        if (err) {
-          console.error("Tokenizer build error:", err);
-          reject(new Error("Error building tokenizer"));
-        } else {
-          resolve(tokenizer);
-        }
+    const tokenizer: kuromoji.Tokenizer<kuromoji.IpadicFeatures> =
+      await new Promise((resolve, reject) => {
+        tokenizerBuilder.build((err, tokenizer) => {
+          if (err) {
+            console.error("Tokenizer build error:", err);
+            reject(new Error("Error building tokenizer"));
+          } else {
+            resolve(tokenizer);
+          }
+        });
       });
-    });
 
     // トークンを解析
     const tokens = tokenizer.tokenize(text);
