0aefdf94d075670dc6b5492581c07407d056ca40
Takuya Takamine <e225717@ie.u-ryukyu.ac.jp>
2024-12-02 21:52:53 +0900

    h-20は大きすぎる用に見えるので2.5rem -> 3remに修正. 縦棒はpaddingのほうが適切なので訂正

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 // components/footer.tsx:
 export default function Footer() {
   return (
-    <footer className="fixed bottom-0 w-full h-20 bg-white border-t border-gray-200 z-50 flex items-center justify-center">
+    <footer className="fixed bottom-0 w-full h-[3rem] bg-white border-t border-gray-200 z-50 flex items-center justify-center">
       {/* Footerの内容 */}
       <p className="text-m">
         &copy;2024 enPiT 開発チームせんばる
-        <span className="mx-2">|</span>
+        <span className="px-2">|</span>
         <Link
           href="https://forms.gle/htZ78h4SnKXnUsmEA"
           className="text-blue-500 underline hover:text-blue-700"
