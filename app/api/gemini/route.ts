1a7a542db64816b78d92d59e1c4b4bc139cba0be
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2024-12-10 15:58:29 +0900

    geminiモデルの変更

--- app/api/gemini/route.ts
+++ app/api/gemini/route.ts
@@ -13,7 +13,7 @@ export async function POST(req: NextRequest) {
     const { _prompt } = await req.json();
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
     const model = genAI.getGenerativeModel({
-      model: "gemini-1.5-pro",
+      model: "gemini-1.5-flash-8b",
     });
     const prompt = _prompt;
     const result = await model.generateContent({
