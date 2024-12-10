a6a68be98dc0207a7a0f5bff34ba4cdfc5cc7ea3
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2024-12-10 15:44:56 +0900

    不要なimportの削除

--- app/transcription/page.tsx
+++ app/transcription/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import { use, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { fileToBase64 } from "../utils/base64";
 import { utils, MicVAD } from "@ricky0123/vad-web";
 import { Switch } from "@/components/ui/switch";
@@ -46,16 +46,17 @@ export default function Home() {
   const [desktopVAD, setDesktopVAD] = useState<MicVAD | undefined>(undefined);
   const speechTextsRef = useRef<string[]>([]);
   const topicRef = useRef<HTMLDivElement>(null);
+  const isUpdateText = useRef(false);
 
   useEffect(() => {
     speechTextsRef.current = speechTexts;
-    let isUpdateText = true;
+    isUpdateText.current = true;
     const intervalId = setInterval(() => {
-      if (!isUpdateText) cleanup();
+      if (!isUpdateText.current) cleanup();
       getTopics();
     }, RE_FETCH_INTERVAL);
     const cleanup = () => {
-      isUpdateText = false;
+      isUpdateText.current = false;
       clearInterval(intervalId);
     };
   }, [speechTexts]);
@@ -313,6 +314,7 @@ export default function Home() {
     const { result } = await response.json();
     setSpeechTexts((prev) => [...prev, result]);
   };
+
 
   const getTopics = async () => {
     const recentTexts = speechTextsRef.current.slice(-15);
